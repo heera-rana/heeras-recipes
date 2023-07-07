a65702b2d14e5fbd40f51a628ffc081be4eb37ed
Heera Rana <heera.rana2@skybettingandgaming.com>
2023-07-07 15:49:13 +0100

    [app-foundation] added app base and imported styling components

---
+++ heeras-recipes/src/components/Popular.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+function Popular() {
+	const [popular, setPopular] = useState([]);
+
+	useEffect(() => {
+		getPopular();
+	}, []);
+
+	const getPopular = async () => {
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+			);
+		const data = await api.json();
+		setPopular(data.recipes);
+	
+	}
+
+	return (
+		<div>
+			{popular.map((recipe) => {
+				return(
+					<Wrapper>
+						<h3> Popular Recipes</h3>
+						{popular.map((recipe) => {
+							return(
+								<Card>
+									<p>{recipe.title}</p>
+								</Card>
+							);
+						})}
+					</Wrapper>
+				);
+			})}
+		</div>
+	);
+}
+
+const Wrapper = styled.div`
+	margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+	min-height: 25rem;
+	border-radius: 2 rem;
+`;
+
+export default Popular;
+
